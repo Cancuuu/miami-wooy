ba349c17068a74cae072fcd6e35476da80fbbb36
Cancu <juancruz.cancu@gmail.com>
2022-05-21 22:38:41 -0300

    littlle details

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,14 +2,37 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 
-const Card = ({ image, title, desc, buttonlabel, path }) => {
+const Card = ({
+  image,
+  title,
+  desc,
+  buttonlabel,
+  path,
+  foundationName,
+  donatedAmount,
+}) => {
   return (
     <div className="flex flex-col justify-between rounded-2xl bg-[#292f55] shadow-lg hover:shadow-2xl hover:bg-[#253082] hover:cursor-pointer transition-all">
       <img src={image} alt="" className="rounded-xl h-64" />
-      <div className="px-8 py-4 flex flex-col justify-center items-center">
-        <h2 className="text-xl text-white">{title}</h2>
-        <hr className="m-2" />
-        <p className="text-white">{desc}</p>
+      <div className="px-8 py-4 flex flex-col justify-center items-center h-full">
+        <h2 className="text-2xl text-white font-bold">{title}</h2>
+
+        <hr className="m-4 w-full" />
+        <div className="flex flex-col justify-center gap-3">
+          <p className="text-teal-300">
+            Address: <span className="text-white">0xa4fs...615232</span>
+          </p>
+          <p className="text-teal-300">
+            {foundationName} address:{" "}
+            <span className="text-white">0xa4fs...615232</span>
+          </p>
+          <p className="text-teal-300">
+            Date: <span className="text-white">05/08/2002</span>
+          </p>
+        </div>
+        <p className="mt-8 text-white font-semibold">
+          Amount donated: <span className="text-teal-300">{donatedAmount}</span>
+        </p>
         {path ? (
           <Link href={path}>
             <button className="mt-4 lg:px-16 px-8 py-2 bg-teal-500 rounded-3xl hover:shadow-2xl hover:bg-teal-300 transition-all">
--- pages/nfts/index.jsx
+++ pages/nfts/index.jsx
@@ -17,21 +17,25 @@ const nfts = () => {
             from the campaigns you help fund!
           </p>
         </div>
-        <div className="shadow-2xl p-8 rounded-2xl w-1/2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <div className="shadow-2xl p-8 rounded-2xl w-[60%] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
           <p className="text-white text-2xl flex">
-            - You have generated in donations:{" "}
+            You have generated in donations:{" "}
             <span className="ml-4 text-3xl text-teal-300 font-bold">$1450</span>
           </p>
           <hr className="my-4  text-white" />
           <p className="text-white text-2xl flex">
-            - Total donations made with Wooy:{" "}
-            <span className="ml-4 text-3xl text-teal-300 font-bold">$1450</span>
+            Total donations made with Wooy:{" "}
+            <span className="ml-4 text-3xl text-teal-300 font-bold">
+              $120,000,000
+            </span>
           </p>
         </div>
         <div className="grid grid-cols-3 gap-7">
           <Card
             image="/donation1.png"
-            title={"Win a ticket"}
+            foundationName={"Impact Market"}
+            donatedAmount="100 cUSD"
+            title={"Win a ticket for the worldcup final x Impact market"}
             desc={"This will be the best prize of the world"}
             buttonlabel={"Claim PoD"}
             path="nfts/1"
@@ -39,7 +43,9 @@ const nfts = () => {
 
           <Card
             image="/donation2.png"
-            title={"Win a ticket"}
+            foundationName={"Camillus House"}
+            donatedAmount="100 Matic"
+            title={"Win this Bored Ape x Camillus House"}
             desc={"This will be the best prize of the world"}
             buttonlabel={"Claim PoD"}
             path="nfts/2"
@@ -47,7 +53,11 @@ const nfts = () => {
 
           <Card
             image="/donation3.png"
-            title={"Win a ticket"}
+            foundationName={"Impact Market"}
+            donatedAmount="100 cUSD"
+            title={
+              "Travel with the Rolling Stones on Tour x Ukranian Cultural Foundation"
+            }
             desc={"This will be the best prize of the world"}
             buttonlabel={"Claim PoD"}
             path="nfts/3"
