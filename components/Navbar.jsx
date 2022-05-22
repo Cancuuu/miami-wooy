bfd2683fd8a703c06acb8fe6db6c18344e19dee5
Cancu <juancruz.cancu@gmail.com>
2022-05-21 21:07:47 -0300

    Added wooy logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className="py-8 px-4 flex justify-between">
-      <h1 className="font-fira text-4xl text-white">Wooy</h1>
+      <Image src="/Wooy.png" width={150} height={60} />
       <ConnectButton />
     </nav>
   );
